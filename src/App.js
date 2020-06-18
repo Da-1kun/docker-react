801711b70cff00180e2a9085500181ad4432dbfb
DAICHI KUN <daichi@DAICHInoiMac.local>
2020-06-18 23:27:37 +0900

    diff test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>I was changed on the feature branch</p>
+        <p>diff test!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
